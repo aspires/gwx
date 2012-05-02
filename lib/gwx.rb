c988789821f55b546ebb1b563c2f69973e0fc6f5
Austin Spires <austinspires@gmail.com>
2012-05-02 11:56:50 -0500

    Created Initializer

--- lib/gwx.rb
+++ lib/gwx.rb
@@ -1,2 +1,29 @@
 require "httparty"
 
+class GoogleWeather
+  include HTTParty
+
+  def initialize(location)
+    result = validate_location(location)
+    unless result.class == String
+      @wx ||= result
+    else
+      return result
+    end
+  end
+
+  private
+
+  def validate_location(location)
+    request = HTTParty.get("http://www.google.com/ig/api?weather=#{location}")
+    response = request["xml_api_reply"]["weather"]
+    case response.include?("problem_cause")
+    when true  
+      result = "Invalid Location"
+    when false
+      result = response
+    end
+    return result
+  end
+
+end
