43b1830892800b7923f5a75f8a3f459bf5b77427
Austin Spires <austinspires@gmail.com>
2012-05-03 10:35:17 -0500

    Tightened Current and Forecast Objects

--- lib/gwx/current_conditions.rb
+++ lib/gwx/current_conditions.rb
@@ -3,12 +3,30 @@ class GoogleWeather::Current
                 :wind_condition
 
   def initialize(wx)
-    @current_conditions ||= wx["current_conditions"]["data"]
-    @conditon ||= @current_condtions["condition"]["data"]
-    @temp_f ||= @current_condtions["temp_f"]["data"].to_i
-    @temp_c ||= @current_condtions["temp_c"]["data"].to_i
-    @humidity ||= @current_condtions["humidity"]["data"]
-    @icon ||= @current_condtions["icon"]["data"]
-    @wind_condition ||= @current_condtions["wind_condition"]["data"]
+    @current_conditions = wx["current_conditions"]["data"]
+  end
+  
+  def condition
+    @conditon = @current_condtions["condition"]["data"]
+  end
+  
+  def temp_f
+    @temp_f = @current_condtions["temp_f"]["data"].to_i
+  end
+  
+  def temp_c
+    @temp_c = @current_condtions["temp_c"]["data"].to_i
+  end
+  
+  def humidity
+    @humidity = @current_condtions["humidity"]["data"]
+  end
+  
+  def icon
+    @icon = @current_condtions["icon"]["data"]
+  end
+  
+  def wind_condition
+    @wind_condition = @current_condtions["wind_condition"]["data"]
   end
 end
--- lib/gwx/forecast_conditions.rb
+++ lib/gwx/forecast_conditions.rb
@@ -2,11 +2,26 @@ class GoogleWeather::Forecast
   attr_accessor :forecast_conditions, :day_of_the_week, :icon, :low, :high, :condition
 
   def initialize(wx)
-    @forecast_conditions ||= wx["forecast_conditions"]["data"]
-    @day_of_the_week ||= @forecast_conditions["day_of_the_week"]["data"]
-    @icon ||= @forecast_conditions["icon"]["data"]
-    @low ||= @forecast_conditions["low"]["data"].to_i
-    @high ||= @forecast_conditions["high"]["data"].to_i
-    @condition ||= @forecast_conditions["condition"]["data"]
+    @forecast_conditions = wx["forecast_conditions"]["data"]
+  end
+  
+  def day_of_the_week
+    @day_of_the_week = @forecast_conditions["day_of_the_week"]["data"]
+  end
+  
+  def icon
+    @icon = @forecast_conditions["icon"]["data"]
+  end
+  
+  def low
+    @low = @forecast_conditions["low"]["data"].to_i
+  end
+  
+  def high
+    @high = @forecast_conditions["high"]["data"].to_i
+  end
+  
+  def condition
+    @condition = @forecast_conditions["condition"]["data"]
   end
 end
